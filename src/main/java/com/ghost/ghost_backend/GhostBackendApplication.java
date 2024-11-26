6c6e03ea50ea0e7915575103a48bd7d42034a12b
alex stone <aharrystone@gmail.com>
2024-11-25 18:54:47 -0600

    initial commit

---
+++ src/main/java/com/ghost/ghost_backend/GhostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ghost.ghost_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghost/ghost_backend/GhostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.ghost_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
